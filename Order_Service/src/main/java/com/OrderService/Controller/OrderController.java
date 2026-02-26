76c5613a8c1b2942c9ea224b3ce5235e1e3ff788
Geeta Khatri <geetakhatri746@gmail.com>
2026-02-26 23:50:00 +0530

    added placeOrder endpoint and InventoryClientconstructor injection

--- Order_Service/src/main/java/com/OrderService/Controller/OrderController.java
+++ Order_Service/src/main/java/com/OrderService/Controller/OrderController.java
@@ -2,15 +2,35 @@ package com.OrderService.Controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+
+import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequestMapping("/orders")
+@RequiredArgsConstructor
 public class OrderController {
-
+	private final InventoryClient inventoryClient;
 	@GetMapping("/hello")
 	public String getHelloOrder() {
 		return "Hello from Order";
 	}
+	@GetMapping("/placeOrder")
+    public String placeOrder(@RequestParam int productCode, 
+                             @RequestParam int quantity) {
+
+        // This is synchronous — thread waits here until Inventory Service responds
+        boolean inStock = inventoryClient.isInStock(productCode, quantity);
+
+        if (inStock) {
+            // Normally call OrderService logic to save order here
+            return "Order placed successfully!";
+        } else {
+            return "Product out of stock!";
+        }
+    }
 }
 
